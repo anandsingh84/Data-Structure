ae739b6bb23771e6a40151c22a09a5d42d7803fd
Anand  Singh <anandsingh84@users.noreply.github.com>
2020-01-29 23:15:17 -0500

    Stack implementation with Array
    Stack implementation with Array - Previous one was with Linked List

---
+++ src/dataType/StackWithArray.js
@@ -0,0 +1,31 @@
+class Stack{
+  constructor(){
+    this.array = [];
+  }
+
+  peek(){
+    return this.array[this.array-1];
+  }
+
+  push(value){
+    this.array.push(value);
+    return this;
+  }
+
+  pop(){    
+    this.array.pop();  
+    return this;
+  }
+
+
+}
+
+const myStack = new Stack();
+myStack.push('Google');
+myStack.push('Udemy');
+myStack.push('Discord');
+//myStack.peek();
+myStack.pop();
+//myStack.pop();
+//myStack.pop();
+
